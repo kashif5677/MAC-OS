192929ca869be872b7a6d26c3f83e57fd7792c98
mdkashif <mdkashifnisar5677@gmail.com>
2026-03-30 23:58:37 +0530

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './app.scss'
 import Dock from './components/Dock'
+import Navbar from './components/Navbar'
 function App() {
 
   return (
    <main>
+    <Navbar/>
     <Dock/>
    </main>
   )
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import './navbar.scss'
+
+function Navbar() {
+  return (
+    <nav>
+        <div className='left'>
+            <div className="apple-icon">
+                <img src="public\navbar-icons\apple-brands-icon.svg" alt="" />
+            </div>
+
+            <div className='nav-item'>
+                <p>Md Kashif Nisar</p>
+            </div>
+            <div className='nav-item'>
+                <p>File</p>
+            </div>
+            <div className='nav-item'>
+                <p>Window</p>
+            </div>
+            <div className='nav-item'>
+                <p>Terminal</p>
+            </div>
+        </div>
+        <div className="right">
+            <div className="nav-icon">
+                <img src="public\navbar-icons\icons8-wifi-16.png" alt="" />
+            </div>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
