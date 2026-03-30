2b698c32186b60f9d916399e43b7de5f29ba9d1f
mdkashif <mdkashifnisar5677@gmail.com>
2026-03-30 22:13:58 +0530

    MAC-OS

---
+++ src/components/Dock.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './dock.scss'
+
+export default function Dock() {
+  return (
+  <footer className='dock'>
+    <div className='icon github'><img src="public\doc-icons\icons8-github-48.png" alt="" /></div>
+    <div className='icon note'><img src="public\doc-icons\notes.svg" alt="" /></div>
+    <div className='icon pdf'><img src="public\doc-icons\pdf.svg" alt="" /></div>
+    <div className='icon calender'><img src="public\doc-icons\icons8-github-48.png" alt="" /></div>
+    <div className='icon'><img src="public\doc-icons\icons8-github-48.png" alt="" /></div>
+    <div className='icon'><img src="public\doc-icons\github.svg" alt="" /></div>
+    <div className='icon'><img src="public\doc-icons\github.svg" alt="" /></div>
+    <div className='icon'><img src="public\doc-icons\github.svg" alt="" /></div>
+  </footer>
+  )
+}
