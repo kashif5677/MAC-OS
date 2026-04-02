ad7a802c8522b5dbd8ed4594f13526e2c600e259
mdkashif <mdkashifnisar5677@gmail.com>
2026-04-02 15:30:05 +0530

    macwindow

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 import './app.scss'
 import Dock from './components/Dock'
 import Navbar from './components/Navbar'
-import MacWindow from './components/windows/MacWindow'
+import Github from './components/windows/Github'
+
 function App() {
 
   return (
    <main>
     <Navbar/>
     <Dock/>
-    <MacWindow>
-      <h1>hello</h1>
-    </MacWindow>
+    <Github/>
    </main>
   )
 }
