e35e06c75f70f971e9d41761618f9773461f3eeb
mdkashif <mdkashifnisar5677@gmail.com>
2026-04-03 21:48:56 +0530

    fetch data

--- src/components/windows/Note.jsx
+++ src/components/windows/Note.jsx
@@ -4,6 +4,13 @@ import Markdown from 'react-markdown'
 
 function Note() {
     const [markdown,setMarkdown]=useState(null)
+
+    useEffect(()=>{
+        fetch('/note.txt')
+        .then(res=>res.text())
+        .then(text=>setMarkdown(text))
+    },[])
+    
   return (
     <div>
       
