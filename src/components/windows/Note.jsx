bd7d0306ad455c53c7603a03e4e5e014a877c892
mdkashif <mdkashifnisar5677@gmail.com>
2026-04-04 15:46:01 +0530

    syntax highligter

--- src/components/windows/Note.jsx
+++ src/components/windows/Note.jsx
@@ -2,7 +2,7 @@ import React,{useEffect, useState} from 'react'
 import Markdown from 'react-markdown'
 import MacWindow from './MacWindow'
 import './note.scss'
-import SyntaxHighlighter from 'react-syntax-highlighter';
+import SyntaxHighlighter from 'react-syntax-highlighter'
 import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
 
@@ -18,7 +18,7 @@ function Note() {
   return (
     <MacWindow>
         <div className='note-window'>
-            {markdown ? <SyntaxHighlighter language='typescript' style={docco}> {markdown}</SyntaxHighlighter>: <p>Loading...</p>}
+            {markdown ? <SyntaxHighlighter language="typescript" style={docco}>{markdown}</SyntaxHighlighter>: <p>Loading...</p>}
         </div>
     </MacWindow>
   )
