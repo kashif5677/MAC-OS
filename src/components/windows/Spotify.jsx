5e95bf32c54c9375536e10b2fa68cfbd9b4d5b5a
mdkashif <mdkashifnisar5677@gmail.com>
2026-04-04 21:31:12 +0530

    spotify src

--- src/components/windows/Spotify.jsx
+++ src/components/windows/Spotify.jsx
@@ -5,7 +5,7 @@ function Spotify() {
   return (
     <MacWindow>
       <div className="spotify-window">
-        <iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/playlist/37i9dQZF1DZ06evO2mZJ0N"></iframe>
+        <iframe data-testid="embed-iframe" style={{borderRadius:"12px"}} src="https://open.spotify.com/embed/album/4jlyYLklV3kTBA6trX3bpj?utm_source=generator"></iframe>
       </div>
     </MacWindow>
   )
