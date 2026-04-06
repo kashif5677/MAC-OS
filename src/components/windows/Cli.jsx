19ff00f636c753e06b054d452937ad60c4671540
mdkashif <mdkashifnisar5677@gmail.com>
2026-04-06 12:56:12 +0530

    cli update

--- src/components/windows/Cli.jsx
+++ src/components/windows/Cli.jsx
@@ -5,14 +5,26 @@ import './cli.scss'
 
 function Cli() {
 
+   const commands = {
+    help: () => 'Available: help, about, projects',
+
+    about: () => "I'm Kashif, Full Stack Developer 🚀",
+
+    projects: () => `
+- AI Trading Dashboard
+- Portfolio Website
+- Chat App
+    `
+  }
 
   return (
     <MacWindow>
       <div className="cli-window">
         <ReactTerminal
-          commands={{}}
-          welcomeMessage="Welcome to Md Kashif's Terminal"
-          prompt="mdkashifprajpati:~$"
+          commands={commands}
+          welcomeMessage={"Welcome to Md Kashif's Terminal"}
+          prompt={"mdkashifprajpati:~$"}
+         promptLabelStyle={{ color: 'green' }}
         />
       </div>
     </MacWindow>
